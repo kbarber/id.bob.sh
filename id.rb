b12fba8639beb88077f82095e4629fdd41cbaf7e
Ken Barber <ken@bob.sh>
2010-09-14 23:26:34 +0100

    Add first view.

--- id.rb
+++ id.rb
@@ -1,12 +1,17 @@
 require 'rubygems'
 require 'sinatra'
 
-enable :sessions
+set :sessions, true
+
+configure :production do
+  set :show_exceptions, false
+  # TODO: instead of exceptions, show a nicer error message.
+end
 
 get '/' do
   redirect '/login'
 end
 
 get '/login' do
-  'Hello world!'
+  erb :login
 end
