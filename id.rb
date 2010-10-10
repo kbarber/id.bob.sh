a182db65499dcc2ac55691323e3d9a48e816fafa
Ken <ken@bob.sh>
2010-10-10 20:47:57 +0000

    Added SSO handling so users who are already authenticated can seamlessly nav to id.bob.sh.

--- id.rb
+++ id.rb
@@ -30,8 +30,37 @@ configure :production do
 end
 
 before do
-  # TODO: need to work on dealing with an SSO key that has already been set
-  # elsewhere.
+
+  # First lets try to establish any prior SSO authentication using crowd.token_key
+  # and looking at our existing sessions.
+
+  crowd_token = request.cookies["crowd.token_key"]
+
+  # If a crowd token is set, and either the username isn't set or 
+  # the cookie crowd.token_key doesn't match the session crowd_token
+  # then lets do a revalidation
+  if crowd_token and (
+      !session.key?(:username) or
+      !session.key?(:crowd_token) or
+      (crowd_token != session[:crowd_token])
+    ) then
+
+    # Find principal and check if its real
+    crowd_principal = Crowd.find_principal_by_token(crowd_token)
+    if crowd_principal == nil then
+      # No such principal, or its expired. Clear the cookie and redirect.
+      response.delete_cookie("crowd.token_key", {
+        :domain => CONF["crowd"]["domain"],
+        :path => "/" })
+      redirect '/login'
+    end
+     
+    # So lets set the session up properly
+    session[:username] = crowd_principal["name"]
+    session[:crowd_token] = crowd_token
+  end
+
+  # Now lets apply any filtering.
 
   # Check if the session is authenticated (ie. username is set) otherwise send
   # the user back to /login.
